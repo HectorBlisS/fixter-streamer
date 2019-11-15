18f65e3a1e8de76a8982480c550de724fc742065
Fixter <fixter@MacBook-Pro-de-Fixter.local>
2019-11-14 22:20:06 -0600

    to aws

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ require('dotenv').config()
 
 const config = {
     rtmp: {
-        // port: 1935,
-        port: process.env.PORT,
+        port: 1935,
+        //port: process.env.PORT,
         chunk_size: 60000,
         gop_cache: true,
         ping: 30,
         ping_timeout: 60
     },
     http: {
-        port: process.env.PORT,
+        port: process.env.PORT || 8000,
         allow_origin: '*'
     }
 };
